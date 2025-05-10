621a869cfd6fe3dd9928acc61fe6c1f984f3c2d8
Bharathi R <bharathi.r@meyicloud.com>
2025-05-10 19:14:59 +0530

    initial setup

--- src/test.py
+++ src/test.py
@@ -1,6 +1,27 @@
-# src/test.py
-def handler(event, context):
-    return {
-        'statusCode': 200,
-        'body': 'Hello, World!'
+import json
+
+def hello(event, context):
+    body = {
+        "message": "Hello function executed successfully!",
+        "input": event
     }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
+
+def world(event, context):
+    body = {
+        "message": "World function executed successfully!",
+        "input": event
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
